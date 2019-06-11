3f284ac847082cbe7f2108023d41d6771c07b0b8
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-11 16:53:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
   <title>Database Test</title>
  </head>
  <body>
- <?php echo '<p>Hello. This is First Cloud App</p>'; 
+ <?php echo '<p>Hello. This is Cloud App</p>'; 
  
  ?> 
+ 	<h1>Hello World</h1>
+ 	<h2>This is Cloud computing class</h2>
+ 	<a href="https://vnexpress.net/">Link to vn express</a>
+ 	<a href="https://www.24h.com.vn/">Link to 24h</a>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
