c63aca4aa344b4c69eb5d969e6b5096dbe34f422
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-13 14:21:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  	<h2>My Name Is Thong</h2>
  	<h2>ID: GCD17150</h2>
  	<h2>Class: GCD0818</h2>
- 	<img class="anhthay" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt="">
+ 	<center><img class="anhthay" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt=""></center>
  	<br>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
