cffdbd252fa38696b8b8e73d4fef72eb4189b160
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-13 14:45:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 			bottom: 230px;
 			left:530px;
  		}
+ 		#Mot{
+ 			position: relative;
+ 			bottom: 120px;
+ 		}
  	</style>
  	<h1>Hello Teacher! This is my Cloud App!</h1>
  	<h2>My Name Is Thong</h2>
@@ -17,6 +21,7 @@
  	<h2>ID: GCD17150</h2>
  	<h2>Class: GCD0818</h2>
  	<img class="anhthay" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt="">
+ 	<section id="Mot">
  	<br/>
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
@@ -25,6 +30,6 @@
      <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
     <br/>
     <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
-
+	</section>
  </body>
 </html>
