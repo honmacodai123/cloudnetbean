e6491cb334439c140d783b57470e231f7dc41e33
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-13 14:08:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
  <?php echo '<p>Hello. This is Cloud App</p>'; 
  
  ?> 
- 	<h1>Hello World</h1>
- 	<h2>This is Cloud computing class</h2>
- 	<a href="https://vnexpress.net/">Link to vn express</a>
- 	<a href="https://www.24h.com.vn/">Link to 24h</a>
+ 	<h1>Hello Teacher!</h1>
+ 	<h2>My Name Is Thong</h2>
+ 	<h2>ID: GCD17150</h2>
+ 	<h2>Class: GCD0818</h2>
+ 	<img src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt="">
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
