d6d6dab510cd770121c8a541fc01282d47cae421
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-13 14:41:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
  		.anhthay{
 			width:20%;
 			position: relative;
-			bottom: 210px;
-			left:500px;
+			bottom: 230px;
+			left:530px;
  		}
  	</style>
  	<h1>Hello Teacher! This is my Cloud App!</h1>
@@ -17,7 +17,7 @@
  	<h2>ID: GCD17150</h2>
  	<h2>Class: GCD0818</h2>
  	<img class="anhthay" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt="">
- 	<br>
+ 	
      <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
