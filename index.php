8e5d43d2c04bb832b2fbc3c00cd7d9bee1ac9fea
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-24 14:38:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,21 @@
  			position: relative;
  			bottom: 140px;
  		}
+        table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        }
+
+        td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+        }
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
  	</style>
  	<h1>Hello Teacher!! </h1>
  	<h2>My Name Is Thong</h2>
@@ -21,16 +36,24 @@
  	<h2>ID: GCD17150</h2>
  	<h2>Class: GCD0818</h2>
  	<img class="anhthay" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/10329285_1054169977931896_7440176948007827970_n.jpg?_nc_cat=110&_nc_oc=AQk5we59AQ7wEH79t6uJJqyY7eNZx4PHAVurJek4LhwrGajjMlU1rBRTzUwiOyWalaE&_nc_ht=scontent.fhan2-4.fna&oh=74a45d33cba6cb3fd1fcbe82efe40274&oe=5D81D519" alt="">
+
  	<section id="Mot">
- 	<br/>
-     <a href="ConnectToDB.php" target="_blank" >Connect database</a>
-     <br/>
-     <a href="InsertData.php" target="_blank" >Insert data to the database</a>
-     <br/>
-     <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
-    <br/>
-    <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
-    <a href="WebDesign.php" target="_blank" >Web Design</a> 
+    <table>
+    <tr>
+ 	
+    <th><a href="ConnectToDB.php" target="_blank" >Connect database</a></th>
+    
+
+    <th><a href="InsertData.php" target="_blank" >Insert data to the database</a></th>
+     
+    <th><a href="UpdateData.php" target="_blank" >Update data to the database</a> </th>
+    
+    <th><a href="DeleteData.php" target="_blank" >Delete data to the database</a></th>
+
+    <th><a href="WebDesign.php" target="_blank" >Web Design</a></th>
+    </tr>
+    </table> 
 	</section>
+
  </body>
 </html>
