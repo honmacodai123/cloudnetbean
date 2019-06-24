7f580a953b8b56dbdf41ea97374706464110a561
Thong Nguyen <thongnqgcd17150@fpt.edu.vn>
2019-06-24 19:42:48 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html>
 <head>
-<title>Women's Fashion a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template | About Us :: w3layouts</title>
+<title>Web Design</title>
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
